702ea3b6f2d516591f6d67a62fe894aad777690f
mark <malei5097@vip.qq.com>
2018-07-24 16:44:06 +0800

    搭建配置中心

---
+++ Config-master/config-client/src/main/java/com/raincloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.raincloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+
+	@Value("${user.name}")
+	String foo;
+	@RequestMapping(value = "/hi",method = RequestMethod.GET)
+	public String hi() {
+		return foo;
+	}
+}
